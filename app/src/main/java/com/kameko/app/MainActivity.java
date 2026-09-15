4f1067e04edec37928897e044c94c76a219e1fdb
prashantyash69-ops <prashant.yash69@gmail.com>
2026-09-15 22:08:41 +0530

    Create app/src/main/java/com/kameko/app/MainActivity.java

---
+++ app/src/main/java/com/kameko/app/MainActivity.java
@@ -0,0 +1,38 @@
+package com.kameko.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setMediaPlaybackRequiresUserGesture(false);
+
+        webView.setWebViewClient(new WebViewClient());
+        
+        // Connects directly to your deployed site
+        webView.loadUrl("https://blaze-x-beta.vercel.app"); 
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView != null && webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
