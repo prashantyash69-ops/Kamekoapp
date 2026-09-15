1ee7dd1f7d792d8a626b7787acc0e1dc06631ab7
prashantyash69-ops <prashant.yash69@gmail.com>
2026-09-15 23:12:34 +0530

    Update MainActivity.java

--- app/src/main/java/com/kameko/app/MainActivity.java
+++ app/src/main/java/com/kameko/app/MainActivity.java
@@ -1,7 +1,10 @@
 package com.kameko.app;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -13,7 +16,17 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
+        // 1. Make the top status bar and bottom navigation bar pure black
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(Color.BLACK);
+        window.setNavigationBarColor(Color.BLACK);
+
         webView = new WebView(this);
+        
+        // 2. Make the WebView background black so it doesn't flash white while loading
+        webView.setBackgroundColor(Color.BLACK); 
+        
         setContentView(webView);
 
         WebSettings webSettings = webView.getSettings();
@@ -22,7 +35,6 @@ public class MainActivity extends Activity {
         webSettings.setMediaPlaybackRequiresUserGesture(false);
 
         webView.setWebViewClient(new WebViewClient());
-        
         webView.loadUrl("https://blaze-x-beta.vercel.app"); 
     }
 
